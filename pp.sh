0f25c4576357059dd193751b9be68f320457d2f8
epitaxy <31850335+epitaxy@users.noreply.github.com>
2017-12-02 22:22:39 +0900

    Create pp.sh

---
+++ pp.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#/usr/bin/ffmpeg -i "$1" -vcodec copy -acodec libfaac -b:a 192k "$(/usr/bin/basename "$1" .ts).mp4" > ~/pp.log
+
+# only convert audio codec from ac3 to aac for fastest conversion as well as for direct streaming in most web browser.
+/usr/bin/ffmpeg -i "$1" -vcodec copy -acodec aac -strict -2 -b:a 192k -movflags +faststart "$1".mp4
+
+# Example convert audio codec from ac3 to aac (128k) 
+#/usr/bin/ffmpeg -i "$1" -vcodec copy -acodec aac -b:a 128k -movflags +faststart -level 4.2 "$1".mp4
+
+#echo "$1" >> ~/parameters
+
+#remove original recodring file (.ts)
+rm "$1"
+
+#/usr/bin/touch ~/"$(/usr/bin/basename "$1" .ts).log"
+
